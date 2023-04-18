335e562604d14658a8f06caf052eb32c201d3526
CarloBattista <carlitobatti@gmail.com>
2023-04-18 12:44:14 +0200

    inserimento testo tramite data e immagine + stile css

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            messageStart: 'Hello Vue!',
+            image: 'https://i.pinimg.com/originals/d4/df/22/d4df220d581fd5bfb4eba9cb23ded3ff.png',
         }
     }
 }).mount('#app')
